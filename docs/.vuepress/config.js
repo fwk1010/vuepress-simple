b83c6c75b230b53793f67ebfc8f9392a1bb4fa4c
fangwk <fwk1010@github.com>
2019-05-11 00:26:26 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+// module.exports = {
+//     title: 'Hello VuePress',
+//     description: 'Just playing around',
+//     themeConfig: {
+//       nav: [
+//         { text: 'Home', link: '/' },
+//         { text: 'About', link: '/about/' },
+//         {
+//           text: 'Languages',
+//           items: [
+//             { text: 'Chinese', link: '/language/chinese' },
+//             { text: 'Japanese', link: '/language/japanese' }
+//           ]
+//         }
+//       ],
+//       sidebar: [
+//         '/',
+//         '/page1',
+//         ['/page2', 'page2']
+//       ]
+//     }
+//   }
