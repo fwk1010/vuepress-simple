b2810cf57d1ae10862f51b53f8fe83a39970c590
fangwk <fwk1010@github.com>
2019-06-22 15:30:41 +0800

    test locales

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,21 @@
+
+
 module.exports = {
-  title: "VuePress",
-  description: "descripion",
+  locales: {
+    "/": {
+        lang: "zh-CN",
+        title: "VuePress 1.x",
+        description: "Vue 驱动的静态网站生成器"
+      },
+    "/en-US/": {
+      lang: "en-US",
+      title: "VuePress 1.x",
+      description: "Vue-powered Static Site Generator"
+    },
+   
+  },
   themeConfig: {
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "About", link: "/about/" },
-      {
-        text: "Languages",
-        items: [
-          { text: "Chinese", link: "/language/chinese" },
-          { text: "Japanese", link: "/language/japanese" }
-        ]
-      }
-    ],
+    nav: [{ text: "Home", link: "/" }, { text: "About", link: "/about/" }],
     sidebar: ["/", "/page1", ["/page2", "page2"]]
   }
 };
