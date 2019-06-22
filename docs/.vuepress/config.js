09c7b8bb286d76cfd263709121b622fb47aeaad1
fangwk <fwk1010@github.com>
2019-06-22 14:03:03 +0800

    update config 1402

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,18 @@
-// module.exports = {
-//     title: 'Hello VuePress',
-//     description: 'Just playing around',
-//     themeConfig: {
-//       nav: [
-//         { text: 'Home', link: '/' },
-//         { text: 'About', link: '/about/' },
-//         {
-//           text: 'Languages',
-//           items: [
-//             { text: 'Chinese', link: '/language/chinese' },
-//             { text: 'Japanese', link: '/language/japanese' }
-//           ]
-//         }
-//       ],
-//       sidebar: [
-//         '/',
-//         '/page1',
-//         ['/page2', 'page2']
-//       ]
-//     }
-//   }
+module.exports = {
+  title: "VuePress",
+  description: "descripion",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+      {
+        text: "Languages",
+        items: [
+          { text: "Chinese", link: "/language/chinese" },
+          { text: "Japanese", link: "/language/japanese" }
+        ]
+      }
+    ],
+    sidebar: ["/", "/page1", ["/page2", "page2"]]
+  }
+};
